7d023747746620d49a496f6f60a207a49a9e2b45
Prometheus <Lotos0208>
2020-12-28 19:06:53 +0300

    Day 5 complete. Немного не понял про второе задание, как вывести данные гетерами, если сетеров нету?

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,42 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setYearOfIssue(2020);
+        car1.setColor("синий");
+        car1.setModel("VW");
+        System.out.println("Год выпуска автомобиля: " + car1.getYearOfIssue() + ". Цвет автомобиля: "
+                + car1.getColor() + ". Модель автомобиля: " + car1.getModel() + ".");
     }
-}
+
+    static class Car {
+        private int yearOfIssue;
+        private String color;
+        private String model;
+
+        public void setYearOfIssue(int carYear) {
+            yearOfIssue = carYear;
+        }
+
+        public int getYearOfIssue() {
+            return yearOfIssue;
+        }
+
+        public void setColor(String carColor) {
+            color = carColor;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setModel(String carModel) {
+            model = carModel;
+        }
+
+        public String getModel() {
+            return model;
+        }
+        }
+    }
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,20 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Moto moto1 = new Moto(2020, "красный", "Yamaha");
+        System.out.println("Год выпуска мотоцикла: " + moto1.yearOfIssue + ". " + "Цвет мотоцикла: "
+                + moto1.color + ". Модель мотоцикла: " + moto1.model + ".");
+    }
 
+    static class Moto {
+        private int yearOfIssue;
+        private String color;
+        private String model;
+
+        public Moto(int yearOfIssue, String color, String model) {
+            this.yearOfIssue = yearOfIssue;
+            this.color = color;
+            this.model = model;
+        }
     }
 }
