19173e6e7d97fbfa45b45fc37378a1f9f495b171
Prometheus <Lotos0208>
2021-01-02 18:57:22 +0300

    Day 8 complete.

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,41 @@
 package day8;
 
+
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 1; i < 20001; i++)
+            sb = sb.append(i).append(" ");
+
+        for (int j = 1; j < 20001; j++) {
+            String n = j + " ";
+            System.out.print(n);
+        }
+        System.out.print(sb);
+        System.out.println("");
+        System.out.println("cold start time " + String.format("%,12d", measure()) + " ns");
+        System.out.println("warmed JRE time " + String.format("%,12d", measure()) + " ns");
+    }
+
+    private static long measure() {
+        long st, en;
+        st = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            ;
+        }
+        en = System.nanoTime();
+        return en - st;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane boeing = new Airplane("Boeing", 2010, 1200, 3000,0);
+        System.out.println(boeing);
+        }
     }
-}
