612cfb820be5d2289a822254d61120dd6e5b3540
Prometheus <Lotos0208>
2021-01-03 21:53:26 +0300

    Day 9 complete.

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,11 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+    Student student = new Student("Сёма", "Покемоны");
+    Teacher teacher = new Teacher("Вениамин Валерьевич", "Математика");
+    System.out.println(student.getGroupName());
+    System.out.println(teacher.getSubjectName());
+    student.printInfo();
+    teacher.printInfo();
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,38 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+
+        System.out.println(calculateRedPerimeter(figures));
+        System.out.println(calculateRedArea(figures));
 
     }
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+        double j = 0;
+        for (Figure figure : figures)
+            if (figure.getColor().equals("Red")) {
+                j += figure.perimeter();
+            }
+        return j;
+    }
+
+    public static double calculateRedArea(Figure[] figures)  {
+        double j = 0;
+        for (Figure figure : figures)
+            if (figure.getColor().equals("Red")) {
+                j += figure.area();
+            }
+        return j;
+    }
 }
+
