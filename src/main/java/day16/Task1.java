ffd617ea0453c4bde64662d344fa1f427876294c
Prometheus <pointofview88@gmail.com>
2021-01-11 01:22:58 +0300

    16 day. Task1 complete.

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -6,40 +6,39 @@ import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-        public class Task1 {
-            public static void main(String[] args) {
-                File fileIn = new File("D:\\Programming\\JavaProjects\\JavaMarathon2020\\src\\main\\java\\day14\\numbers.txt");
-                System.out.println(printResult(fileIn));
-            }
+public class Task1 {
+    public static void main(String[] args) {
+        File fileIn = new File("D:\\Programming\\JavaProjects\\JavaMarathon2020\\src\\main\\java\\day14\\numbers.txt");
+        System.out.println(printResult(fileIn));
+    }
 
-            public static File printResult(File file) {
-                File result = new File("D:\\Programming\\JavaProjects\\JavaMarathon2020\\src\\main\\java\\day16\\result.txt");
-                double sum = 0;
-                int count = 0;
-                DecimalFormat dF = new DecimalFormat( "#.###" );
+    public static File printResult(File file) {
+        File result = new File("D:\\Programming\\JavaProjects\\JavaMarathon2020\\src\\main\\java\\day16\\result.txt");
+        double sum = 0;
+        int count = 0;
+        DecimalFormat dF = new DecimalFormat( "#.###" );
 
-                try {
-                    Scanner scanner = new Scanner(file);
-                    PrintWriter pw = new PrintWriter(result);
+        try {
+            Scanner scanner = new Scanner(file);
+            PrintWriter pw = new PrintWriter(result);
 
-                    while (scanner.hasNextLine()) {
-                        String[] lineIn = scanner.nextLine().split(" ");
+            while (scanner.hasNextLine()) {
+                String[] lineIn = scanner.nextLine().split(" ");
 
-                        for (String number : lineIn) {
-                            sum += Double.parseDouble(number);
-                            count++;
-                        }
-                        double value = sum / count;
-                        pw.println(value + " --> " + dF.format(value));
-                    }
-                    pw.close();
-                    scanner.close();
-                    return result;
-                } catch (FileNotFoundException e) {
-                    System.out.println("Файл не найден!");
+                for (String number : lineIn) {
+                    sum += Double.parseDouble(number);
+                    count++;
                 }
-                return null;
-
+                double value = sum / count;
+                pw.println(value + " --> " + dF.format(value));
             }
+            pw.close();
+            scanner.close();
+            return result;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден!");
         }
+        return null;
 
+    }
+}
