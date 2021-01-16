a4ce9ebf94d2c8bdf52a9b93d1b55db0c5a12313
Prometheus <pointofview88@gmail.com>
2021-01-17 00:51:56 +0300

    18 day complete.

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,15 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+            int[] numbers = {432, 529, 173, 432, 823, 462};
+        System.out.println(recursionSum(numbers,0));
+    }
 
+
+
+    public static int recursionSum(int[] Array, int index) {
+        if (index==(Array.length) - 1)
+            return Array[index];
+        return Array[index] + recursionSum(Array, index+1);
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,20 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        int numbers = 755756754;
 
+        System.out.println(count7(numbers));
+
+    }
+
+
+    public static int count7(int n){
+        if(n == 0)
+            return 0;
+
+        if(n % 10 == 7)
+            return 1 + count7(n / 10);
+        else
+            return count7(n / 10);
     }
 }
