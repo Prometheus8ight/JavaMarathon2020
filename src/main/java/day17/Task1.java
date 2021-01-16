efc69337324ff8d700322570c30a854b1863164a
Prometheus <pointofview88@gmail.com>
2021-01-17 00:51:21 +0300

    17 day complete.

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,14 @@
 package day17;
 
+import day9.Task2.Figure;
+
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] figures = {ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+        ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK};
 
+        for(ChessPiece chessPiece: figures){
+            System.out.print(chessPiece.getFigureDesignation() + " ");
+        }
     }
 }
